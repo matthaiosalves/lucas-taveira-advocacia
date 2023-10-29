5045b4859de25ad7edf272184ffe264cc91da6b7
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2023-10-29 17:05:25 -0300

    :sparkles: criado o componente de areas de atuação

---
+++ home.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: Página Inicial
+*/
+
+get_header();
+?>
+
+
+<?php get_footer(); ?>
---
+++ template-parts/areas-de-atuacao/index.php
@@ -0,0 +1,29 @@
+<section class="areasDeAtuacao">
+  <div class="container-fluid">
+
+    <div class="row">
+      <div class="boxTitleAndDescription">
+        <h2 class="title">Áreas de atuação</h2>
+        <p class="descrition">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.</p>
+      </div>
+    </div>
+    <hr />
+
+    <div class="row">
+      <div class="boxContentAreas">
+
+        <figure class="boxImage">
+          <img src="" alt="">
+        </figure>
+
+        <div class="boxTitleAndDescriptionAreas">
+          <h2 class="title">Maior experiência em Direito do Consumidor</h2>
+          <p class="description">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+          </p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
