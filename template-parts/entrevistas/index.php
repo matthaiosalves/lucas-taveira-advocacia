f3f63c1afe5b615618a082e4cb96eeb831cbf781
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2023-10-29 17:13:54 -0300

    :sparkles: criado o componente de entrevistas

---
+++ template-parts/entrevistas/index.php
@@ -0,0 +1,23 @@
+<section class="entrevistas">
+  <div class="container-fluid">
+    <div class="row">
+
+      <div class="boxContentEntrevistas">
+
+        <div class="boxTitleAndDescriptionEntrevistas">
+          <h2 class="title">Maior experiência em Direito do Consumidor</h2>
+          <hr>
+          <p class="description">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+          </p>
+        </div>
+
+        <figure class="boxImage">
+          <img src="" alt="">
+        </figure>
+
+      </div>
+
+    </div>
+  </div>
+</section>
