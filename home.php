14d82d66223ac38389b22f2b868ec267caf46659
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2023-11-02 14:00:50 -0300

    :ajustes

--- home.php
+++ home.php
@@ -7,6 +7,7 @@ get_header();
 ?>
 
 <?php
+require_once get_template_directory() . '/template-parts/banner-home/index.php';
 require_once get_template_directory() . '/template-parts/entrevistas/index.php';
 require_once get_template_directory() . '/template-parts/areas-de-atuacao/index.php';
 require_once get_template_directory() . '/template-parts/conheca-nossa-equipe/index.php';
---
+++ template-parts/banner-home/index.php
@@ -0,0 +1,2 @@
+<section class="banner-home" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/banner-home.webp');">
+</section>
