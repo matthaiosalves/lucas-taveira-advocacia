bed7cfa62cbe77e91fa2a64ee8b47c4f1b5551ca
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2024-02-27 21:03:18 -0300

    :sparkles: AJUSTE NA LOGICA PARA QUE O CODIGO SÓ APAREÇA NA PAGINA DE VIDEOS

--- footer.php
+++ footer.php
@@ -258,7 +258,7 @@ $footer = get_field('footer', 'options');
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/33ee87c64c.js" crossorigin="anonymous"></script>
-<?php if (is_page('videos')) : ?>
+<?php if (is_page('videos') || (is_page() && $post->post_parent == get_page_by_path('videos')->ID)) : ?>
   <script>
     (function($) {
       $(document).ready(function() {
